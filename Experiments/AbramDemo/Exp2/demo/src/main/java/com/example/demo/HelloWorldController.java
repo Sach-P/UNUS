489e7fd9dda7990a0c7e5a0c1c24f583f4bf35fe
ademo24 <ademo24@iastate.edu>
2022-09-07 15:27:52 -0500

    Tried some HTML syntax stuff

--- Experiments/AbramDemo/Exp2/demo/src/main/java/com/example/demo/HelloWorldController.java
+++ Experiments/AbramDemo/Exp2/demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -10,4 +10,11 @@ public class HelloWorldController
 	{  
 		return "<html><h1 style=\"color: red;\">Hello <br> World</h1></html>";  
 	}  
+	
+	@RequestMapping("/bye")
+	public String bye() 
+	{
+		return "<html><p style=\"font-family: Cursive;\">Goodbye for now :(<br></p> <img src=\"https://image.petmd.com/files/2021-11/sad-dog.jpg\" "
+				+ "alt=\"sad dog\" height=\"500\" width=\"700\"/> </html>";
+	}
 }  
