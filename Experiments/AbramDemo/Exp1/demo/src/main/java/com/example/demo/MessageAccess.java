30730b7e956fa53ecc3e621a065a883d559ad7e5
ademo24 <ademo24@iastate.edu>
2022-09-08 15:24:37 -0500

    Made something that saves and stores names, they can be deleted aswell

---
+++ Experiments/AbramDemo/Exp1/demo/src/main/java/com/example/demo/MessageAccess.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+public class MessageAccess {
+	
+	private String[] messages = new String[10];
+	private int index;
+	
+	public MessageAccess() {
+		for(int i = 0; i < 10; i++) {
+			messages[i] = " ";
+		}
+	}
+	
+	public void addMessage(String s) {
+		messages[index] = s;
+		if(index<9) { index++;}
+	}
+	
+	public String getMessage(int i) {
+		if(i < 0) {return messages[0]; }
+		if(i > 9) {return messages[9]; }
+		return messages[i];
+	}
+	
+	public String[] allMessages() {
+		return messages;
+	}
+	
+	public void deleteMessage(int n) {
+		index--;
+		if(n < 0) { n = 0; }
+		if(n > 9) { n = 9; }
+		for(int i = n; i < 9; i++) {
+			messages[i] = messages[i+1];
+		}
+		messages[9] = " ";
+	}
+
+}
