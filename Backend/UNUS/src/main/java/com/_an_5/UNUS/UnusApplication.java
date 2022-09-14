438643ebe7ebd63eb4c31a4f4d91c616e27d8788
Sachin Patel <sachpatel1220@gmail.com>
2022-09-14 14:49:06 -0500

    created base project for backend

---
+++ Backend/UNUS/src/main/java/com/_an_5/UNUS/UnusApplication.java
@@ -0,0 +1,13 @@
+package com._an_5.UNUS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnusApplication.class, args);
+	}
+
+}
---
+++ Backend/UNUS/src/test/java/com/_an_5/UNUS/UnusApplicationTests.java
@@ -0,0 +1,13 @@
+package com._an_5.UNUS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
