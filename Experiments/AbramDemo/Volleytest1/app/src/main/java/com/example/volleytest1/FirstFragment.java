3a6c35e80e3ac5eb2ab1250f575e1f87592583fa
ademo24 <ademo24@iastate.edu>
2022-09-26 11:24:01 -0500

    volley test works

--- Experiments/AbramDemo/Volleytest1/app/src/main/java/com/example/volleytest1/FirstFragment.java
+++ Experiments/AbramDemo/Volleytest1/app/src/main/java/com/example/volleytest1/FirstFragment.java
@@ -5,16 +5,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.volleytest1.databinding.FragmentFirstBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private EditText username;
+    private EditText password;
+    private TextView text;
 
     @Override
     public View onCreateView(
@@ -30,22 +42,67 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        username = (EditText) view.findViewById(R.id.username);
+        password = (EditText) view.findViewById(R.id.password);
+
+        text = (TextView) view.findViewById(R.id.textview_first);
+
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, new SecondFragment()).commit();
+                sendLoginPostRequest(username.getText().toString(), password.getText().toString());
             }
         });
 
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText editName = (EditText) binding.editTextName;
+                EditText editName = (EditText) binding.username;
                 String name = editName.getText().toString();
-
-
+                text.setText(username.getText());
             }
         });
+    }
+
+    private void sendLoginPostRequest(String username, String password){
+        try{
+
+            JSONObject requestBody = new JSONObject();
+            requestBody.put("username", username);
+            requestBody.put("password", password);
+
+
+            JsonObjectRequest request = new JsonObjectRequest(
+                    Request.Method.POST,
+                    getString(R.string.postman_mock_server_url),
+                    requestBody,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                if (response.getString("verification").equals("passed")){
+                                    text.setText("login success");
+                                } else {
+                                    text.setText("login falied");
+                                }
+                            } catch (JSONException ex) {
+                                text.setText("error");
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    }
+            );
+
+            Volley.newRequestQueue(requireContext()).add(request);
+
+        } catch(JSONException ex) {
+
+        }
     }
 
     @Override
