0e5dd8b19f7b0ae71486b04e8cdc7e7bd025dca0
Isaac Blandin <32471937+IsaacB8588@users.noreply.github.com>
2022-10-04 21:38:12 -0500

    added user singleton object and associated Friends object

---
+++ Frontend/Unus/app/src/main/java/com/example/unus/Friend.java
@@ -0,0 +1,11 @@
+package com.example.unus;
+
+public class Friend {
+    private int userID;
+    private String username;
+
+    public Friend(int userID, String username){
+        this.userID = userID;
+        this.username = username;
+    }
+}
