5d5990527778348b2c402ee5c5cfabb78eaf9931
Sachin Patel <sachpatel1220@gmail.com>
2022-11-03 22:11:38 -0500

    global chat

--- Backend/UNUS/src/main/java/com/_an_5/UNUS/Lobbies/WebSocketConfig.java
+++ Backend/UNUS/src/main/java/com/_an_5/UNUS/Lobbies/WebSocketConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
-public class LobbySocketConfig {
+public class WebSocketConfig {
     @Bean
     public ServerEndpointExporter serverEndpointExporter(){
         return new ServerEndpointExporter();
