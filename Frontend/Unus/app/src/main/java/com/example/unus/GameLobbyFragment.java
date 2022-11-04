0eb16ab00dc248da5c0da18fb7d6a18b0f811e13
Isaac Blandin <32471937+IsaacB8588@users.noreply.github.com>
2022-11-04 13:10:16 -0500

    added programmatic UI logic

--- Frontend/Unus/app/src/main/java/com/example/unus/GameLobbyFragment.java
+++ Frontend/Unus/app/src/main/java/com/example/unus/GameLobbyFragment.java
@@ -8,6 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Fragment used to handle the game lobby menu
@@ -15,6 +25,8 @@ import android.widget.LinearLayout;
 public class GameLobbyFragment extends Fragment {
 
     View view;
+    int[] players;
+
 
     public GameLobbyFragment() {
         // Required empty public constructor
@@ -36,7 +48,8 @@ public class GameLobbyFragment extends Fragment {
         return view;
     }
 
-    private void addPlayerPlate(int playerID){
+    //create the UI display of a player in the lobby
+    private void addPlayerPlate(int playerID, String username){
         //create horizontal linear layout
         LinearLayout plate = new LinearLayout(view.getContext());
         plate.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
@@ -44,7 +57,32 @@ public class GameLobbyFragment extends Fragment {
         plate.setOrientation(LinearLayout.HORIZONTAL);
         plate.setPadding(30,10,30,30);
         plate.setBackgroundColor(getResources().getColor(R.color.dark_gray));
+    }
 
+    public void addPlayer(int id){
 
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                "http://coms-309-029.class.las.iastate.edu:8080/user/"+Integer.toString(id),
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+                            addPlayerPlate(response.getInt("id"), response.getString("username"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }
+        );
+        Volley.newRequestQueue(requireContext()).add(request);
     }
 }
