69a461a9b8a87139edb7f6eb4fbe18adb0ce5152
ademo24 <ademo24@iastate.edu>
2022-11-03 23:59:15 -0500

    Leaderboards now work

--- Frontend/Unus/app/src/main/java/com/example/unus/Friend.java
+++ Frontend/Unus/app/src/main/java/com/example/unus/Friend.java
@@ -3,10 +3,21 @@ package com.example.unus;
 public class Friend {
     private int userID;
     private String username;
+    private int games_played;
+    private int games_won;
 
     public Friend(int userID, String username){
         this.userID = userID;
         this.username = username;
+        this.games_played = 0;
+        this.games_won = 0;
+    }
+
+    public Friend(int userID, String username, int played, int won){
+        this.userID = userID;
+        this.username = username;
+        this.games_played = played;
+        this.games_won = won;
     }
 
     public String getUsername(){
@@ -15,4 +26,6 @@ public class Friend {
     public int getUserID(){
         return userID;
     }
+    public int getGamesPlayed(){return games_played;}
+    public int getGamesWon(){return games_won;}
 }
--- Frontend/Unus/app/src/main/java/com/example/unus/LeaderboardFragment.java
+++ Frontend/Unus/app/src/main/java/com/example/unus/LeaderboardFragment.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Space;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -30,12 +32,18 @@ import java.util.ListIterator;
 
 public class LeaderboardFragment extends Fragment {
 
+    private View view;
     private Button back;
     private Button global;
     private Button friends;
     private Button played;
     private Button won;
+    private TextView stat_name;
+    public boolean isGlobal = true;
+    public boolean isPlayed = true; //I know this is terrible for scaling but I'll fix it later
+    private LinearLayout board;
     private List<Friend> userList;
+    private List<Friend> friendList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,17 +54,22 @@ public class LeaderboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         userList = new ArrayList<Friend>();
+        friendList = new ArrayList<Friend>();
+        getUsers();
+        /*
+        Something in here is so bugged but I can't for the life of me find it, it works and that;s all that matters for now
+         */
 
-        View view = inflater.inflate(R.layout.fragment_leaderboard, container, false);
+        view = inflater.inflate(R.layout.fragment_leaderboard, container, false);
 
         back = (Button) view.findViewById(R.id.back);
         global = (Button) view.findViewById(R.id.global_list);
         friends = (Button) view.findViewById(R.id.friends_list);
         played = (Button) view.findViewById(R.id.games_played);
         won = (Button) view.findViewById(R.id.games_won);
+        board = (LinearLayout) view.findViewById(R.id.users);
+        stat_name = (TextView) view.findViewById(R.id.stat_name);
 
-
-        getUsers();
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,8 +81,62 @@ public class LeaderboardFragment extends Fragment {
         won.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(isPlayed) {
+                    won.setBackgroundColor(view.getResources().getColor(R.color.bright_purple));
+                    won.setTextColor(view.getResources().getColor(R.color.yellow));
+                    played.setBackgroundColor(view.getResources().getColor(R.color.yellow));
+                    played.setTextColor(view.getResources().getColor(R.color.bright_purple));
+                    isPlayed = false;
+                    stat_name.setText("Games Won: ");
+                    sortGamesWon();
+                    displayUsers((isGlobal) ? userList : friendList, false);
+                }
+            }
+        });
 
-                back.setText(userList.get(0).getUsername());
+        played.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isPlayed) {
+                    played.setBackgroundColor(view.getResources().getColor(R.color.bright_purple));
+                    played.setTextColor(view.getResources().getColor(R.color.yellow));
+                    won.setBackgroundColor(view.getResources().getColor(R.color.yellow));
+                    won.setTextColor(view.getResources().getColor(R.color.bright_purple));
+                    isPlayed = true;
+                    stat_name.setText("Games Played: ");
+                    sortGamesPlayed();
+                    displayUsers((isGlobal) ? userList : friendList, true);
+                }
+            }
+        });
+
+        global.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isGlobal) {
+                    global.setBackgroundColor(view.getResources().getColor(R.color.bright_purple));
+                    global.setTextColor(view.getResources().getColor(R.color.yellow));
+                    friends.setBackgroundColor(view.getResources().getColor(R.color.yellow));
+                    friends.setTextColor(view.getResources().getColor(R.color.bright_purple));
+                    isGlobal = true;
+                    stat_name.setText((isPlayed) ? "Games Played: " : "Games Won: ");
+                    displayUsers(userList, isPlayed);
+                }
+            }
+        });
+
+        friends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isGlobal) {
+                    friends.setBackgroundColor(view.getResources().getColor(R.color.bright_purple));
+                    friends.setTextColor(view.getResources().getColor(R.color.yellow));
+                    global.setBackgroundColor(view.getResources().getColor(R.color.yellow));
+                    global.setTextColor(view.getResources().getColor(R.color.bright_purple));
+                    isGlobal = false;
+                    stat_name.setText((isPlayed) ? "Games Played: " : "Games Won: ");
+                    displayUsers(friendList, isPlayed);
+                }
             }
         });
 
@@ -88,12 +155,24 @@ public class LeaderboardFragment extends Fragment {
                     public void onResponse(JSONArray response) {
                         try {
                             for( int i = 0; i < response.length(); i++) {
-                                userList.add(new Friend(response.getJSONObject(i).getInt("id"), response.getJSONObject(i).getString("username")));
+                                userList.add(new Friend(response.getJSONObject(i).getInt("id"), response.getJSONObject(i).getString("username"),
+                                        response.getJSONObject(i).getInt("gamesPlayed"), response.getJSONObject(i).getInt("gamesWon")));
+                                for(int j = 0; j < UserData.getInstance().getFriendsList().length; j++) {
+                                    if(UserData.getInstance().getFriendsList()[j].getUserID() == response.getJSONObject(i).getInt("id"))
+                                        friendList.add(new Friend(response.getJSONObject(i).getInt("id"), response.getJSONObject(i).getString("username"),
+                                                response.getJSONObject(i).getInt("gamesPlayed"), response.getJSONObject(i).getInt("gamesWon")));
+                                }
+                                if(UserData.getInstance().getUserID() == response.getJSONObject(i).getInt("id"))
+                                    friendList.add(new Friend(response.getJSONObject(i).getInt("id"), response.getJSONObject(i).getString("username"),
+                                            response.getJSONObject(i).getInt("gamesPlayed"), response.getJSONObject(i).getInt("gamesWon")));
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
 
+                        sortGamesPlayed();
+                        stat_name.setText("Games Played: ");
+                        displayUsers(userList, true);
                     }
                 },
                 new Response.ErrorListener() {
@@ -103,5 +182,72 @@ public class LeaderboardFragment extends Fragment {
                 }
         );
         Volley.newRequestQueue(requireContext()).add(request);
+
+    }
+
+    private void sortGamesPlayed() {
+        List<Friend> temp = new ArrayList<Friend>();
+        temp.add(userList.get(0));
+        for(int i = 1; i < userList.size(); i++) {
+            int j;
+            for (j = 0; j < temp.size() && userList.get(i).getGamesPlayed() < temp.get(j).getGamesPlayed(); j++);
+            temp.add(j, userList.get(i));
+        }
+        userList = temp;
+
+        List<Friend> temp2 = new ArrayList<Friend>();
+        temp2.add(friendList.get(0));
+        for(int i = 1; i < friendList.size(); i++) {
+            int j;
+            for (j = 0; j < temp2.size() && friendList.get(i).getGamesPlayed() < temp2.get(j).getGamesPlayed(); j++);
+            temp2.add(j, friendList.get(i));
+        }
+        friendList = temp2;
+    }
+
+    private void sortGamesWon() {
+        List<Friend> temp = new ArrayList<Friend>();
+        temp.add(userList.get(0));
+        for(int i = 1; i < userList.size(); i++) {
+            int j;
+            for (j = 0; j < temp.size() && userList.get(i).getGamesWon() < temp.get(j).getGamesWon(); j++);
+            temp.add(j, userList.get(i));
+        }
+        userList = temp;
+
+        List<Friend> temp2 = new ArrayList<Friend>();
+        temp2.add(friendList.get(0));
+        for(int i = 1; i < friendList.size(); i++) {
+            int j;
+            for (j = 0; j < temp2.size() && friendList.get(i).getGamesWon() < temp2.get(j).getGamesWon(); j++);
+            temp2.add(j, friendList.get(i));
+        }
+        friendList = temp2;
+    }
+
+    private void displayUsers(List<Friend> list, boolean played) {
+        board.removeViews(0, board.getChildCount());
+        for(int i = 0; i < list.size(); i++) {
+            LinearLayout layout = new LinearLayout(view.getContext());
+            TextView tv = new TextView(view.getContext());
+            tv.setLayoutParams( new ViewGroup.LayoutParams(500, 100));
+            tv.setText(list.get(i).getUsername());
+            tv.setTextColor(view.getResources().getColor(R.color.yellow));
+            tv.setTextSize(25);
+
+            Space sp = new Space(view.getContext());
+            sp.setLayoutParams( new ViewGroup.LayoutParams(100, 100));
+
+            TextView stats = new TextView(view.getContext());
+            stats.setLayoutParams( new ViewGroup.LayoutParams(400, 100));
+            stats.setText(""+((played)?list.get(i).getGamesPlayed():list.get(i).getGamesWon()));
+            stats.setTextColor(view.getResources().getColor(R.color.yellow));
+            stats.setTextSize(25);
+
+            layout.addView(tv);
+            layout.addView(sp);
+            layout.addView(stats);
+            board.addView(layout);
+        }
     }
 }
