a2cad5bda636efe31b0b745a947c025991f30a28
Isaac Blandin <32471937+IsaacB8588@users.noreply.github.com>
2022-11-23 16:13:59 -0600

    Made lobby hosting asynchronous to prevent need for button double click when response is slow

--- Frontend/Unus/app/src/main/java/com/example/unus/MainMenuFragment.java
+++ Frontend/Unus/app/src/main/java/com/example/unus/MainMenuFragment.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -79,8 +80,6 @@ public class MainMenuFragment extends Fragment {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
-                startHostedLobby();
             }
         });
 
@@ -149,7 +148,7 @@ public class MainMenuFragment extends Fragment {
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-
+                        startHostedLobby();
                     }
                 },
                 new Response.ErrorListener() {
