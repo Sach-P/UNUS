e8a7ef290af41eda2e0f05bd812ca69209e8e05f
Isaac Blandin <32471937+IsaacB8588@users.noreply.github.com>
2022-12-07 23:17:54 -0600

    made Friend and Team tests have 100% coverage

--- Frontend/Unus/app/src/test/java/com/example/unus/FriendTypeTest.java
+++ Frontend/Unus/app/src/test/java/com/example/unus/FriendTypeTest.java
@@ -5,13 +5,81 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * @author Abe Demo
+ * tests all of the methods in the Friend object
+ *
+ * @author Isaac Blandin
  */
 public class FriendTypeTest {
+
+    /**
+     * tests all of the constructors
+     */
     @Test
-    public void FriendConstructorCorrect() {
-        Friend friend = new Friend(12, "newFriend");
-        assertEquals(friend.getUsername(), "newFriend");
-        assertEquals(friend.getUserID(), 12);
+    public void testConstructors(){
+        Friend f1 = new Friend(12, "friend1");
+        assertEquals(f1.getUsername(), "friend1");
+        assertEquals(f1.getUserID(), 12);
+        assertNull(f1.getPassword());
+        assertEquals(f1.getGamesPlayed(), 0);
+        assertEquals(f1.getGamesWon(), 0);
+
+        Friend f2 = new Friend(14, "friend2", 3, 2);
+        assertEquals(f2.getUserID(), 14);
+        assertEquals(f2.getUsername(), "friend2");
+        assertNull(f2.getPassword());
+        assertEquals(f2.getGamesPlayed(), 3);
+        assertEquals(f2.getGamesWon(), 2);
+
+        Friend f3 = new Friend(16, "friend3", "password", 4, 3);
+        assertEquals(f3.getUserID(), 16);
+        assertEquals(f3.getUsername(), "friend3");
+        assertEquals(f3.getPassword(), "password");
+        assertEquals(f3.getGamesPlayed(), 4);
+        assertEquals(f3.getGamesWon(), 3);
+    }
+
+    /**
+     * tests the getUsername method
+     */
+    @Test
+    public void testGetUserName(){
+        Friend f = new Friend(16, "alex", "password", 4, 3);
+        assertEquals(f.getUsername(), "alex");
+    }
+
+    /**
+     * tests the getUserID method
+     */
+    @Test
+    public void testGetUserID(){
+        Friend f = new Friend(420, "alex", "password", 4, 3);
+        assertEquals(f.getUserID(), 420);
+    }
+
+    /**
+     * tests the getGamesPlayed method
+     */
+    @Test
+    public void testGetGamesPlayer(){
+        Friend f = new Friend(420, "alex", "password", 7, 3);
+        assertEquals(f.getGamesPlayed(), 7);
+    }
+
+    /**
+     * tests the getGamesWon method
+     */
+    @Test
+    public void testGamesWon(){
+        Friend f = new Friend(420, "alex", "password", 7, 23);
+        assertEquals(f.getGamesWon(), 23);
+    }
+
+    /**
+     * tests the getGamesWon method
+     */
+    @Test
+    public void testGetPassword(){
+        Friend f = new Friend(420, "alex", "12345", 7, 23);
+        assertEquals(f.getPassword(), "12345");
     }
 }
--- Frontend/Unus/app/src/test/java/com/example/unus/TeamTypeTest.java
+++ Frontend/Unus/app/src/test/java/com/example/unus/TeamTypeTest.java
@@ -4,20 +4,93 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 /**
- * @author Abe Demo
+ * tests the methods in the Team Object
+ *
+ * @author Isaac Blandin
  */
 public class TeamTypeTest {
+
+    /**
+     * tests the primary constructor
+     */
     @Test
-    public void TeamConstructorCorrect() {
-        Team temp = new Team("team1", 1, 0);
-        assertEquals(temp.getWins(), 0);
+    public void testConstructor() {
+        Team t = new Team("team1", 1, 0);
+        assertEquals(t.getName(), "team1");
+        assertEquals(t.getId(), 1);
+        assertEquals(t.getWins(), 0);
     }
 
+    /**
+     * tests the alternative constructor
+     */
     @Test
-    public void TeamConstructorCorrectAlt() {
-        Team temp = new Team(1, "team2", null, null);
-        assertEquals(temp.getName(), "team2");
+    public void testConstructorAlt() {
+        Friend f1 = new Friend(1, "leader");
+        Friend f2 = new Friend(2, "notLeader");
+        ArrayList<Friend> friends = new ArrayList<>();
+        friends.add(f1);
+        friends.add(f2);
+
+        Team t = new Team(1, "weFeastWeEat", f1, friends);
+        assertEquals(t.getName(), "weFeastWeEat");
+        assertEquals(t.getId(), 1 );
+        assertEquals(t.getLeader(), f1);
+        assertEquals(t.getPlayers(), friends);
     }
 
+    /**
+     * tests the getName method
+     */
+    @Test
+    public void testGetName(){
+        Team t = new Team("daBoys", 23, 7000);
+        assertEquals(t.getName(), "daBoys");
+    }
+
+    /**
+     * tests the getId method
+     */
+    @Test
+    public void testGetId(){
+        Team t = new Team("daBoys", 23, 7000);
+        assertEquals(t.getId(), 23);
+    }
+
+    /**
+     * tests the getWins method
+     */
+    @Test
+    public void testGetWins(){
+        Team t = new Team("daBoys", 23, 7000);
+        assertEquals(t.getWins(), 7000);
+    }
+
+    /**
+     * tests the getLeader method
+     */
+    @Test
+    public void testGetLeader(){
+        Friend f1 = new Friend(1, "leader");
+
+        Team t = new Team(1, "backAtItAgain", f1, null);
+        assertEquals(t.getLeader(), f1);
+    }
+
+    /**
+     * test getPlayers method
+     */
+    @Test
+    public void testGetPlayers(){
+
+        ArrayList<Friend> friends = new ArrayList<>();
+        friends.add(new Friend(1, "leader"));
+        friends.add(new Friend(2, "notLeader"));
+
+        Team t = new Team(1, "runningOutOfFunnyNames", null, friends);
+        assertEquals(t.getPlayers(), friends);
+    }
 }
